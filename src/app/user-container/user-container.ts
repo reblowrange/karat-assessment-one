918fccd57b16edbe58c9a4f54ea89375f727832b
reblowrange <reblowrange@gmail.com>
2026-05-05 18:52:55 +0530

    Task #6 - Async Pipe - Subscribe to an restService->getUsers() Observable named as 'data$' in a user-container component template using the 'async' pipe to avoid manual unsubscription in the TypeScript file.

--- src/app/user-container/user-container.ts
+++ src/app/user-container/user-container.ts
@@ -2,23 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { UserCardComponent } from '../user-card-component/user-card-component';
 import { User } from '../models/common.model';
 import { RestService } from '../service/rest.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../libs/material-module';
 
 @Component({
   selector: 'app-user-container',
-  imports: [UserCardComponent],
+  imports: [CommonModule, MaterialModule, UserCardComponent],
   templateUrl: './user-container.html',
   styleUrl: './user-container.scss',
 })
 export class UserContainer implements OnInit {
   public user = { name: 'Swapnil', email: 'swapnil@ltm.com' } as User;
   public randomGeneratedId = NaN;
+  public data$: Observable<User[]>;
 
-  constructor(private restService: RestService) {}
+  constructor(public restService: RestService) {
+    this.data$ = this.restService.getUsers();
+  }
 
   ngOnInit(): void {
-    this.restService.getUsers().subscribe((res) => {
-      console.log(res);
-    });
   }
 
   public onGeneratedId(randomId: number): void {
