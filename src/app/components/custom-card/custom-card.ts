7f48dbb8ed48c886521e98d526517ddd0b6d0518
reblowrange <reblowrange@gmail.com>
2026-05-06 17:01:14 +0530

    Task #16 - Content Projection - Created a 'custom-card' component template that uses to allow users to pass custom HTML into the card's body.

---
+++ src/app/components/custom-card/custom-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-card',
+  imports: [],
+  templateUrl: './custom-card.html',
+  styleUrl: './custom-card.scss',
+})
+export class CustomCard {}
--- src/app/components/user-container/user-container.ts
+++ src/app/components/user-container/user-container.ts
@@ -1,7 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UserCardComponent } from '../user-card-component/user-card-component';
 
-
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 
@@ -10,12 +9,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MaterialModule } from '@app/libs/material-module';
 import { User } from '@app/models/common.model';
 import { TruncatePipe } from '@app/pipes/truncate-pipe';
-import { RestService } from '@app/service/rest.service';
 import { AuthService } from '@app/service/auth.service';
+import { RestService } from '@app/service/rest.service';
+import { CustomCard } from '../custom-card/custom-card';
 
 @Component({
   selector: 'app-user-container',
-  imports: [CommonModule, MaterialModule, UserCardComponent, TruncatePipe],
+  imports: [CommonModule, MaterialModule, UserCardComponent, TruncatePipe, CustomCard],
   templateUrl: './user-container.html',
   styleUrl: './user-container.scss',
 })
