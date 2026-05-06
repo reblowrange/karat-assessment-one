e0adbebf7eb8c4dba395260bb4da5dd8992988ec
reblowrange <reblowrange@gmail.com>
2026-05-06 14:06:06 +0530

    Task #11 - Accessing Route Params - In product details component retrieve the 'id' parameter from the current active route using ActivatedRoute.

--- src/app/product-details.component/product-details.component.ts
+++ src/app/product-details.component/product-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details.component',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
 })
-export class ProductDetailsComponent {}
+export class ProductDetailsComponent {
+  productId!: string | null;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.paramMap.subscribe((params) => {
+      this.productId = params.get('id');
+    });
+  }
+}
